281f6607c7befe7b028dcab7caf15584810015d1
andoanminhkhanh <24a4043059@hvnh.edu.vn>
2024-06-11 22:26:38 +0700

    Lucky

--- ThucTapChuyenNganh/Forms/Chitiethoadonbanhang.Designer.cs
+++ ThucTapChuyenNganh/Forms/Chitiethoadonbanhang.Designer.cs
@@ -514,11 +514,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(567, 16);
+            this.label1.Location = new System.Drawing.Point(434, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(303, 32);
+            this.label1.Size = new System.Drawing.Size(432, 32);
             this.label1.TabIndex = 51;
-            this.label1.Text = "HÓA ĐƠN BÁN HÀNG";
+            this.label1.Text = "CHI TIẾT HÓA ĐƠN BÁN HÀNG";
             // 
             // Chitiethoadonbanhang
             // 
